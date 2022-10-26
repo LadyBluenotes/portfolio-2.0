d95e0109f5fd80a42b57d919b5bf8c22d5e9733f
gerrardsarah <gerrardsarah@gmail.com>
2022-10-26 15:19:24 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Sarah Gerrard - Software Developer</title>
+        <meta name="description" content="Sarah Gerrard software developer information" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Sarah Gerrard - Software Developer
+        </h1>
+
+        
+      </main>
+
+      <footer className={styles.footer}
+        sx={{
+          display: 'block'
+        }}>
+        <span> &copy; Copyright <strong>Sarah Gerrard</strong> {new Date().getFullYear()}. All Rights Reserved.</span>
+
+      </footer>
+    </div>
+  )
+}
