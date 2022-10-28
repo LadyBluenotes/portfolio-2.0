ebbda9e88c68ce1b5ab9804faf251f3faade32e2
gerrardsarah <gerrardsarah@gmail.com>
2022-10-28 16:31:57 -0700

    form

--- pages/success.js
+++ pages/success.js
@@ -1,3 +1,5 @@
-export default function Success() {
-    return <div>Form successfully submitted!</div>
+const success = () => {
+    return (
+    <div>Form successfully submitted!</div>
+    )
   }
