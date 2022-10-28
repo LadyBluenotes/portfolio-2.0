2eb54bf39006b06eccbdb2b1e221caa9835da163
gerrardsarah <gerrardsarah@gmail.com>
2022-10-28 16:52:00 -0700

    form

--- pages/contact.js
+++ pages/contact.js
@@ -29,7 +29,7 @@ const contact = () => {
 
       <form method="POST"
         name="contact-form"
-        action="/success"
+        action="sarahgerrard.netlify.app/success"
         data-netlify="true">
         <input type="text" name="name" placeholder="Name" required />
         <input type="text" name="email" placeholder="Email" required />
